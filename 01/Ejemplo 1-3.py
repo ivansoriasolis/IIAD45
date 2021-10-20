5482276b38480f9bccf0d6e8890297887fe96432
ivansoriasolis <soria.solis.ivan@googlemail.com>
2021-10-19 19:31:40 -0500

    corregido 1.3

--- 01/Ejemplo 1-3.py
+++ 01/Ejemplo 1-3.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Ejemplo 1-2 Funcion que cuenta la cantida de ocurrencias 
+Ejemplo 1-3 Funcion que cuenta la cantida de ocurrencias 
 """
 def contar(data, target):
-    if not isinstance(data, (list, tuple, set)):
-        raise TypeError("No es iterable")
     n = 0
     for item in data:
         if item == target:
             n += 1
     return n
 
-lista = "[1, 3, 4, 5]"
+lista = [1, 3, 4, 5]
 buscado = 3
 
-try:
-    print(contar(lista, buscado))
-except:
-    print("Necesita un iterable")
+print(contar(lista, buscado))
