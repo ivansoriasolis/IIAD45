d4df42297e7a98aa3f00fed1a335f36b90d770cf
ivansoriasolis <soria.solis.ivan@googlemail.com>
2021-11-12 18:00:53 -0500

    reinas primera parate

--- 04/reinas.py
+++ 04/reinas.py
@@ -24,6 +24,26 @@ def ocupadoO(i, j, tablero):
         return False
     return tablero[i][j] or ocupadoO(i, j-1, tablero)
 
+def ocupadoNE(i, j, tablero):
+    if i < 0 or j > len(tablero[0])-1:
+        return False
+    return tablero[i][j] or ocupadoNE(i-1, j+1, tablero)
+
+def ocupadoSE(i, j, tablero):
+    if i > len(tablero)-1 or j > len(tablero[0])-1:
+        return False
+    return tablero[i][j] or ocupadoSE(i+1, j+1, tablero)
+
+def ocupadoNO(i, j, tablero):
+    if i < 0 or j < 0:
+        return False
+    return tablero[i][j] or ocupadoNO(i-1, j-1, tablero)
+
+def ocupadoSO(i, j, tablero):
+    if i > len(tablero)-1 or j < 0:
+        return False
+    return tablero[i][j] or ocupadoSO(i+1, j-1, tablero)
+
 ancho = 8
 alto = 8
 tablero = [ [0 for n in range(ancho)] for m in range(alto)]
