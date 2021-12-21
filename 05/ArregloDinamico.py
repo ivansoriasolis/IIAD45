f6919007d4fbb1d2576a9e0e56207b71552d54ff
ivansoriasolis <soria.solis.ivan@googlemail.com>
2021-12-21 18:43:33 -0500

    remove corregido

--- 05/ArregloDinamico.py
+++ 05/ArregloDinamico.py
@@ -52,9 +52,10 @@ class ArregloDinamico:
     def remove(self, value):
         """Remover la primera ocurrencia de value"""
         for k in range(self._n):
-            for j in range(k, self._n-1):
-                self._A[j] = self._A[j+1]
-            self._A[self._n - 1] = None
-            self._n -=1
-            return
+            if self._A[k] == value:
+                for j in range(k, self._n-1):
+                    self._A[j] = self._A[j+1]
+                self._A[self._n - 1] = None
+                self._n -=1
+                return
         raise ValueError('valor no encontrado')
